d67a6c258092cd435bd869ffbe39d5ec8f063c82
tarhippa <114419309+tarhippa@users.noreply.github.com>
2022-09-26 17:44:51 +0300

    Add files via upload

---
+++ planet_info.js
@@ -0,0 +1,86 @@
+//epoch J2000 data at 2451545.0 JD
+epoch = 2451545.0
+
+var planet = {
+
+	1:{  
+		name:'Mercury',
+		a:0.38709893,
+		e:0.20563069,
+		i:7.00487,
+		Omega:48.33167,
+		tildeOmega:77.45645,
+		MeanLongitude:252.25084
+	},
+	2:{
+		name:'Venus',
+		a:0.72333199,
+		e:0.00677323,
+		i:3.39471,
+		Omega:76.68069,
+		tildeOmega:131.53298,
+		MeanLongitude:181.97973
+	},
+	3:{
+		name:'Earth',
+		a:1.00000011,
+		e:0.01671022,
+		i:0.00005,
+		Omega:174.8731758,
+		tildeOmega:102.94719,
+		MeanLongitude:100.46435
+	},
+	4:{
+		name:'Mars',
+		a:1.52366231,
+		e:0.09341233 ,
+		i:1.85061,
+		Omega:49.57854,
+		tildeOmega:336.04084,
+		MeanLongitude:355.45332
+	},
+	5:{
+		name:'Jupiter',
+		a:5.20336301,
+		e:0.04839266 ,
+		i:1.30530,
+		Omega:100.55615,
+		tildeOmega:14.75385,
+		MeanLongitude:34.40438
+	},
+	6:{
+		name:'Saturn',
+		a:9.53707032,
+		e:0.05415060 ,
+		i:2.48446,
+		Omega:113.71504,
+		tildeOmega:92.43194,
+		MeanLongitude:49.94432
+	},
+	7:{
+		name:'Uranus',
+		a:19.19126393,
+		e:0.04716771 ,
+		i:0.76986,
+		Omega:74.22988,
+		tildeOmega:170.96424,
+		MeanLongitude:313.23218
+	},
+	8:{
+		name:'Neptune',
+		a:30.06896348,
+		e:0.00858587,
+		i:1.76917,
+		Omega:131.72169,
+		tildeOmega:44.97135,
+		MeanLongitude:304.88003
+	}/*
+		Pluto,
+		39.48168677,
+		0.24880766 ,
+		17.14175,
+		110.30347,
+		224.06676,
+		238.92881
+		*/
+};
